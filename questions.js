b7cbba6519952c5e7ad36dcb50ba7f6bbb382fbe
Devaasish <75482391+Devaasish@users.noreply.github.com>
2021-04-24 19:09:16 +0530

    Initial commit

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "2-d figure which has two end points",
+    answer: "Line Segment",
+    options: [
+      "Ray",
+      "Line Segment",
+      "Line",
+      "Circle"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are Supplementary angles?",
+    answer: "a + b = 180",
+    options: [
+      "a + b = 80",
+      "a + b = 290",
+      "a + b = 90",
+      "a + b = 180"
+    ]
+  },
+    {
+    numb: 3,
+    question: "x - 55 = 180, x= ?",
+    answer: "235",
+    options: [
+      "235",
+      "533",
+      "128",
+      "234"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is a line?",
+    answer: "<----->",
+    options: [
+      "All the above",
+      "------>",
+      "-------",
+      "<----->"
+    ]
+  },
+    {
+    numb: 5,
+    question: "a + b + c = 180?",
+    answer: "Supplementary",
+    options: [
+      "Supplementary",
+      "Linear Pair",
+      "Both 1 and 2",
+      "Complementary"
+    ]
+  }
+];
